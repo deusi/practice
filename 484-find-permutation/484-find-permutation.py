f6fbe3868839bb53ec2b5d121f8953d3b7d7e3f5
Denis Rybkin <rybki001@umn.edu>
2022-05-03 17:28:22 -0400

    Time: 121 ms (45.41%), Space: 15.3 MB (54.89%) - LeetHub

--- 484-find-permutation/484-find-permutation.py
+++ 484-find-permutation/484-find-permutation.py
@@ -3,6 +3,9 @@ class Solution:
         num, i = 1, 1
         perm = []
         for c in s:
+            # if encountered 'I', push nums and decrease nums
+            # That way we account for previous 'D'
+            # and push only one num if no 'D' were encountered before
             if c == 'I':
                 tmp = num + 1
                 while num >= i:
@@ -11,6 +14,7 @@ class Solution:
                 i, num = tmp, tmp
             else:
                 num += 1
+        # push any remaining nums to perm
         while num >= i:
             perm.append(num)
             num -= 1
