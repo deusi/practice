005a6edb0a08dcacbc6365438c57397a53e332bb
Denis Rybkin <rybki001@umn.edu>
2022-05-04 23:27:19 -0400

    Time: 53 ms (20.03%), Space: 14 MB (76.44%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.py
@@ -16,10 +16,15 @@ class MyStack:
     # pop in O(n)
     def pop(self) -> int:
         # push all but 2 elements from queue1 to queue2
-        while len(self.q1) > 1:
+        while len(self.q1) > 2:
+            e = self.q1.popleft()
+            self.q2.append(e)
+        # push second elements to queue2 and make this element on top
+        if len(self.q1) > 1:
             e = self.q1.popleft()
             self.q2.append(e)
             self.topElem = e
+        # pop the last element
         e = self.q1.popleft()
         
         self.q2, self.q1 = self.q1, self.q2
