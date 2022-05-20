8c5d6da0426c50b25cc34adc59cefcd9840df4bf
Denis Rybkin <rybki001@umn.edu>
2022-05-19 20:13:33 -0400

    Time: 541 ms (66.77%), Space: 18.7 MB (26.91%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -1,40 +1,25 @@
-# COPY SOLUTION FOR STREAK
-
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        rows = len(matrix)
-        if rows == 0:
-            return 0
+        if not matrix or not matrix[0]:
+            return 0 
         
-        cols = len(matrix[0])
-        indegree = [[0 for x in range(cols)] for y in range(rows)] 
-        directions = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+        m,n = len(matrix) - 1, len(matrix[0]) - 1
+        directions = [(1, 0), (-1, 0), (0, 1), (0 ,-1)]
+        maxPath = 0
         
-        for x in range(rows):
-            for y in range(cols):
-                for direction in directions:
-                    nx, ny = x + direction[0], y + direction[1]
-                    if nx >= 0 and ny >= 0 and nx < rows and ny < cols:
-                        if matrix[nx][ny] < matrix[x][y]:
-                            indegree[x][y] += 1
-                            
-        queue = []
-        for x in range(rows):
-            for y in range(cols):
-                if indegree[x][y] == 0:
-                    queue.append((x, y))
-    
-        path_len = 0
-        while queue:
-            sz = len(queue)
-            for i in range(sz):
-                x, y = queue.pop(0)
-                for direction in directions:
-                    nx, ny = x + direction[0], y + direction[1]
-                    if nx >= 0 and ny >= 0 and nx < rows and ny < cols:
-                        if matrix[nx][ny] > matrix[x][y]:
-                            indegree[nx][ny] -= 1
-                            if indegree[nx][ny] == 0:
-                                queue.append((nx, ny))
-            path_len += 1
-        return path_len 
+        @cache
+        def dfs(row, col):
+            curPath = 0
+            for first, second in directions:
+                nRow, nCol = row + first, col + second
+                if nRow < 0 or nRow > m or nCol < 0 or nCol > n or matrix[nRow][nCol] <= matrix[row][col]:
+                    continue
+                curPath = max(curPath, dfs(nRow, nCol))
+            curPath += 1
+            return curPath
+        
+        for i in range(m + 1):
+            for j in range(n + 1):
+                maxPath = max(maxPath, dfs(i, j))
+                
+        return maxPath
