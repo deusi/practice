7a8ac475d3831b1de4f180da206cefb5015ab5bd
Denis Rybkin <rybki001@umn.edu>
2022-06-01 12:14:53 -0400

    Time: 39 ms (92.39%), Space: 14.1 MB (27.03%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -1,4 +1,6 @@
 class Solution:
+    # Runtime Complexity: O(n), since we are going through each number in the array
+    # Space Complexity: O(1), since we are modifying array in-place
     def runningSum(self, nums: List[int]) -> List[int]:
         for i in range(1, len(nums)):
             nums[i] = nums[i] + nums[i - 1]
