f90624f75fceb9e85eb237857f8e08f0065f196e
Denis Rybkin <rybki001@umn.edu>
2022-06-03 19:47:59 -0400

    Time: 2553 ms (30.99%), Space: 25.4 MB (19.88%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
@@ -1,5 +1,5 @@
 class NumMatrix:
-
+    # Time & Space Compexity: O(m*n) due to initialization and storage of matrix
     def __init__(self, matrix: List[List[int]]):
         self.numMatrix = [[0 for j in range(len(matrix[0]) + 1)] for i in range(len(matrix) + 1)]
         print(self.numMatrix)
@@ -7,7 +7,7 @@ class NumMatrix:
             for j in range(len(matrix[0])):
                 self.numMatrix[i + 1][j + 1] = self.numMatrix[i + 1][j] + self.numMatrix[i][j + 1] + matrix[i][j] - self.numMatrix[i][j]
         print(self.numMatrix)
-
+    # Time Complexity: O(1)
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         return self.numMatrix[row2 + 1][col2 + 1] - self.numMatrix[row1][col2 + 1] - self.numMatrix[row2 + 1][col1] + self.numMatrix[row1][col1]
 
