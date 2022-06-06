888ede6281a9f17953c70511938a8caa0135881a
Denis Rybkin <rybki001@umn.edu>
2022-06-06 10:36:07 -0400

    Time: 217 ms (45.77%), Space: 29.5 MB (69.35%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -5,15 +5,19 @@
 #         self.next = None
 
 class Solution:
+    # Time Complexity: O(m + n)
+    # Space Complexity: O(1)
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         lenA, lenB = 0, 0
         nodeA, nodeB = headA, headB
         while nodeA:
             lenA += 1
             nodeA = nodeA.next
+            
         while nodeB:
             lenB += 1
             nodeB = nodeB.next
+            
         nodeA, nodeB = headA, headB
         while lenA > lenB:
             nodeA = nodeA.next
@@ -21,10 +25,11 @@ class Solution:
         while lenB > lenA:
             nodeB = nodeB.next
             lenB -= 1
+            
         while lenA > 0:
             if nodeA is nodeB:
                 return nodeA
-            nodeA = nodeA.next
-            nodeB = nodeB.next
+            nodeA, nodeB = nodeA.next, nodeB.next
             lenA -= 1
+            
         return None
