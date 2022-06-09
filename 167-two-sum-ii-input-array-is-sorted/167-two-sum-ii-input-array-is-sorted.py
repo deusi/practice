69f9494028c920d5d531bac62d2a1b42a1c80686
Denis Rybkin <rybki001@umn.edu>
2022-06-08 20:33:19 -0400

    Time: 208 ms (33.30%), Space: 14.9 MB (89.05%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,4 +1,6 @@
 class Solution:
+    # Runtime Complexity: O(n)
+    # Space Complexity: O(1)
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left, right = 0, len(numbers) - 1
         while left < right:
