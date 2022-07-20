a4a7274d32bc505e4a32404a3f174eddaf86b642
Denis Rybkin <rybki001@umn.edu>
2022-07-19 22:46:47 -0400

    Time: 41 ms (77.01%), Space: 13.9 MB (82.20%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -1,4 +1,6 @@
 class Solution:
+    # Runtime Complexity: O(m), where m = len(t)
+	# Space Complexity: O(1) - no extra space used
     def isSubsequence(self, s: str, t: str) -> bool:
         if len(s) == 0:
             return True
@@ -6,6 +8,7 @@ class Solution:
         for char in t:
             if s[cur] == char:
                 cur += 1
+                # no need to keep iterating if the full subsequence matched
                 if cur == len(s):
                     return True
         return cur == len(s)
