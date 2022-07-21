ccac5725a02801e1d6a1cc5f5ebf20ed886fd04f
Denis Rybkin <rybki001@umn.edu>
2022-07-20 22:22:41 -0400

    Time: 40 ms (94.59%), Space: 14.3 MB (44.39%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -4,9 +4,8 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         s_map, t_map = {}, {}
         for s_char, t_char in zip(s, t):
-            if s_char not in s_map:
+            if s_char not in s_map and t_char not in t_map:
                 s_map[s_char] = t_char
-            if t_char not in t_map:
                 t_map[t_char] = s_char
             if s_map.get(s_char, 0) != t_char or t_map.get(t_char, 0) != s_char:
                 return False
