cc119cd237ac29e961dfda96369843336f5e863a
Denis Rybkin <rybki001@umn.edu>
2022-07-25 21:24:18 -0400

    Time: 73 ms (5.68%), Space: 13.8 MB (60.97%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -2,6 +2,8 @@
 # def isBadVersion(version: int) -> bool:
 
 class Solution:
+    # Runtime Complexity: O(log n)
+    # Space Complexity: O(1)
     def firstBadVersion(self, n: int) -> int:
         low, high = 1, n
         while low <= high:
