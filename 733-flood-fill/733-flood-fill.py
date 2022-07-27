ae8d6a7c9d28e08fb5f3f530fb3b87bc635b7932
Denis Rybkin <rybki001@umn.edu>
2022-07-27 15:55:45 -0400

    Time: 99 ms (68.20%), Space: 13.9 MB (99.36%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.py
@@ -0,0 +1,16 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        m, n = len(image), len(image[0])
+        oldColor = image[sr][sc]
+        image[sr][sc] = color
+        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+        queue = collections.deque([[sr, sc]])
+        while queue:
+            r, c = queue.popleft()
+            for d1, d2 in directions:
+                if r+d1 >= m or c+d2 >= n or r+d1 < 0 or c+d2 < 0 or image[r+d1][c+d2] == color:
+                    continue
+                if image[r+d1][c+d2] == oldColor:
+                    image[r+d1][c+d2] = color
+                    queue.append([r+d1, c+d2])
+        return image
