51b37dbb9a36ed3ff9bddf52ccbcdbcb21bf2abd
Denis Rybkin <rybki001@umn.edu>
2022-07-28 16:40:16 -0400

    Time: 44 ms (69.79%), Space: 13.8 MB (53.81%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    # Runtime Complexity: O(n)
+    # Space Complexity: O(n), used to store the results of previous calls to fib
+    @lru_cache
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        return self.fib(n - 1) + self.fib(n - 2)
