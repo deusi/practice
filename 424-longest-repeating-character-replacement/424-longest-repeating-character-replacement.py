a2d3957db24ca0726dcf40fdc1c5172ebe3b4bc7
Denis Rybkin <rybki001@umn.edu>
2022-07-30 15:41:59 -0400

    Time: 198 ms (57.45%), Space: 13.9 MB (93.51%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,16 +1,14 @@
 class Solution:
     # Runtime Complexity: O(n)
-    # Space Complexity: O(1)
+    # Space Complexity: O(26)
     def characterReplacement(self, s: str, k: int) -> int:
         alphabet = [0 for _ in range(26)]
-        longest, left, aSum, curMax = 0, 0, 0, 0
+        longest, left, maxVal = 0, 0, 0
         for right in range(len(s)):
             alphabet[ord(s[right]) - 65] += 1
-            aSum += 1
-            curMax = max(curMax, alphabet[ord(s[right]) - 65])
-            if aSum - curMax > k:
+            maxVal = max(maxVal, alphabet[ord(s[right]) - 65])
+            if (right - left + 1) - maxVal > k:
                 alphabet[ord(s[left]) - 65] -= 1
-                aSum -= 1
                 left += 1
             longest = max(longest, right - left + 1)
         return longest
