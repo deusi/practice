7a9c39c142ddbec1444dd7f445cc54fba9a9ce0a
Denis Rybkin <rybki001@umn.edu>
2022-07-31 15:08:49 -0400

    Time: 48 ms (83.59%), Space: 13.9 MB (79.40%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.py
+++ 299-bulls-and-cows/299-bulls-and-cows.py
@@ -1,10 +1,6 @@
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
-        sMap, gMap = collections.Counter(secret), collections.Counter(guess)
-        iMap = {}
-        for s in sMap:
-            if s in gMap:
-                iMap[s] = min(sMap[s], gMap[s])
+        iMap = collections.Counter(secret) & collections.Counter(guess)
         bulls, cows = 0, 0
         for i in range(len(secret)):
             if secret[i] == guess[i]:
