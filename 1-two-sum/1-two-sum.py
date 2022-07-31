ea51780ad9b9fc667b60a0b2b6e3857c98c3633e
Denis Rybkin <rybki001@umn.edu>
2022-07-30 20:42:48 -0400

    Time: 106 ms (57.06%), Space: 15.1 MB (50.44%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    # Runtime Complexity: O(n)
+    # Space Complexity: O(1)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nMap = {}
+        for i, n in enumerate(nums):
+            if target - n in nMap:
+                return [nMap[target - n], i]
+            nMap[n] = i
