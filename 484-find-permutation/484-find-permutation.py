2f412f81df3a50114d5dcd8cb5fdac66915aca5d
Denis Rybkin <rybki001@umn.edu>
2022-07-31 21:53:12 -0400

    Time: 154 ms (30.51%), Space: 15.2 MB (64.41%) - LeetHub

--- 484-find-permutation/484-find-permutation.py
+++ 484-find-permutation/484-find-permutation.py
@@ -1,21 +1,14 @@
 class Solution:
     def findPermutation(self, s: str) -> List[int]:
-        num, i = 1, 1
         perm = []
+        dQueue = [1]
+        count = 2
         for c in s:
-            # if encountered 'I', push nums and decrease nums
-            # That way we account for previous 'D'
-            # and push only one num if no 'D' were encountered before
             if c == 'I':
-                tmp = num + 1
-                while num >= i:
-                    perm.append(num)
-                    num -= 1
-                i, num = tmp, tmp
-            else:
-                num += 1
-        # push any remaining nums to perm
-        while num >= i:
-            perm.append(num)
-            num -= 1
+                while dQueue:
+                    perm.append(dQueue.pop())
+            dQueue.append(count)
+            count += 1
+        while dQueue:
+            perm.append(dQueue.pop())
         return perm
