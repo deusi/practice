f1769f57783510d2abb89eb025f9acb023ca737e
Denis Rybkin <rybki001@umn.edu>
2022-08-02 14:28:11 -0400

    Time: 68 ms (8.49%), Space: 13.9 MB (74.61%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -0,0 +1,23 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def nextValidChar(str, idx):
+            backspaces = 0
+            while idx >= 0:
+                if str[idx] == '#':
+                    backspaces += 1
+                else:
+                    if backspaces == 0:
+                        break
+                    backspaces -= 1
+                idx -= 1
+            return idx
+        
+        idxS, idxT = len(s) - 1, len(t) - 1
+        while idxS > -1 or idxT > -1:
+            nextS, nextT = nextValidChar(s, idxS), nextValidChar(t, idxT)
+            if nextS < 0 and nextT < 0:
+                return True
+            elif nextS < 0 or nextT < 0 or s[nextS] != t[nextT]:
+                return False
+            idxS, idxT = nextS - 1, nextT - 1
+        return True
