cda7620ee5f29fa9d97d28c6ea3cb4f721697af8
Denis Rybkin <rybki001@umn.edu>
2022-08-01 20:53:29 -0400

    Time: 235 ms (78.88%), Space: 18.6 MB (80.82%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,6 +1,8 @@
 from heapq import *
 
 class Solution:
+    # Runtime Complexity: O(n^2 * k log k)
+    # Space Complexity O(k)
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         maxHeap = []
         for i in range(len(matrix)):
