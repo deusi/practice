70ea1d4763f3b11b3baa5377c9c371d45598bed3
Denis Rybkin <rybki001@umn.edu>
2022-08-07 13:44:45 -0400

    Time: 1170 ms (28.35%), Space: 182.2 MB (10.31%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -10,15 +10,15 @@ class Solution:
             num = 0
             vowel = vowels[idx]
             if vowel == 'a':
-                num += bt(1, curN + 1)
+                num += bt(1, curN + 1) % (10**9 + 7)
             elif vowel == 'e':
-                num += bt(0, curN + 1) + bt(2, curN + 1)
+                num += bt(0, curN + 1) + bt(2, curN + 1) % (10**9 + 7)
             elif vowel == 'i':
-                num += bt(0, curN + 1) + bt(1, curN + 1) + bt(3, curN + 1) + bt(4, curN + 1)
+                num += bt(0, curN + 1) + bt(1, curN + 1) + bt(3, curN + 1) + bt(4, curN + 1) % (10**9 + 7)
             elif vowel == 'o':
-                num += bt(2, curN + 1) + bt(4, curN + 1)
+                num += bt(2, curN + 1) + bt(4, curN + 1) % (10**9 + 7)
             else: 
-                num += bt(0, curN + 1)
+                num += bt(0, curN + 1) % (10**9 + 7)
             return num % (10**9 + 7)
         ans = 0
         for v in range(len(vowels)):
