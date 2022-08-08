7b6c4437c2bbb252b2b88c3a8b94a00afae15e51
Denis Rybkin <rybki001@umn.edu>
2022-08-07 23:04:39 -0400

    Time: 52 ms (38.03%), Space: 13.8 MB (73.37%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.py
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.py
@@ -5,28 +5,31 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # Runtime Complexity: O(n log n) - n to traverse each level of leaf nodes , log n to go over tree height 
+    # assuming tree is balanced, worst case could be degenerated to O(n^2)
+    # Space Complexity: O(n) 
     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
         output = []
         while root.left or root.right:
             queue = collections.deque([root])
-            leaves = []
+            leafs = []
             while queue:
                 node = queue.popleft()
                 leftnode = node.left
                 rightnode = node.right
                 if leftnode:
                     if not leftnode.left and not leftnode.right:
-                        leaves.append(leftnode.val)
+                        leafs.append(leftnode.val)
                         node.left = None
                     else:
                         queue.append(leftnode)
                 if rightnode:
                     if not rightnode.left and not rightnode.right:
-                        leaves.append(rightnode.val)
+                        leafs.append(rightnode.val)
                         node.right = None
                     else:
                         queue.append(rightnode)
-            output.append(leaves)
+            output.append(leafs)
         output.append([root.val])
         return output
                 
