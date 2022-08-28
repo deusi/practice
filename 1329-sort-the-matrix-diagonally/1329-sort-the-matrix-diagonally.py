62667b177657de70eada62433f503f0fe283ef22
Denis Rybkin <rybki001@umn.edu>
2022-08-28 15:56:29 -0400

    Time: 123 ms (62.79%), Space: 14.1 MB (99.53%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
@@ -0,0 +1,26 @@
+class Solution:
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        for j in range(len(mat[0])):
+            sortedDiagonal = []
+            for i in range(len(mat)):
+                if j+i >= len(mat[0]):
+                    break
+                sortedDiagonal.append(mat[i][j+i])
+            sortedDiagonal.sort()
+            for i in range(0, len(mat)):
+                if j+i >= len(mat[0]):
+                    break
+                mat[i][j+i] = sortedDiagonal[i]
+        for i in range(1, len(mat)):
+            sortedDiagonal = []
+            for j in range(len(mat[0])):
+                if j+i >= len(mat):
+                    break
+                sortedDiagonal.append(mat[i+j][j])
+            sortedDiagonal.sort()
+            for j in range(len(mat[0])):
+                if j+i >= len(mat):
+                    break
+                mat[i+j][j] = sortedDiagonal[j]
+        return mat
+                
