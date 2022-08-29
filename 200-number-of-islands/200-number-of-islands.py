38a611b7c914c0888f275b28598999c884e887ff
Denis Rybkin <rybki001@umn.edu>
2022-08-29 11:35:17 -0400

    Time: 630 ms (19.81%), Space: 16.1 MB (97.80%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,26 +1,17 @@
 class Solution:
-    # Runtime Complexity: O(m*n) - because of nested for loop
-    # Space Complexity: O(m*n), when queue deals with one huge island
     def numIslands(self, grid: List[List[str]]) -> int:
-        iCount = 0
-        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
-        queue = collections.deque()
-        # O(m*n), but it would only happen once (one big island), hence overall numIslands would also be m*n
-        # O(m*n)
-        def visitIsland(row, col):
-            grid[row][col] = '2'
-            queue.append([row, col])
-            while queue:
-                r, c = queue.popleft()
-                for d1, d2 in directions:
-                    nR, nC = r+d1, c+d2
-                    if nR >= len(grid) or nC >= len(grid[0]) or nR < 0 or nC < 0 or grid[nR][nC] != '1':
-                        continue
-                    grid[nR][nC] = '2'
-                    queue.append([nR, nC])
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    iCount += 1
-                    visitIsland(i, j)
-        return iCount
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        islandCount = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == '1':
+                    islandCount += 1
+                    grid[r][c] = '2'
+                    queue = collections.deque([(r, c)])
+                    while queue:
+                        cR, cC = queue.popleft()
+                        for d1, d2 in dirs:
+                            if cR + d1 >= 0 and cR + d1 < len(grid) and cC + d2 >= 0 and cC + d2 < len(grid[0]) and grid[cR + d1][cC + d2] == '1':
+                                grid[cR + d1][cC + d2] = '2'
+                                queue.append((cR + d1, cC + d2))
+        return islandCount
