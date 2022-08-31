efbdbb99649cbdcb4e7898bd23ab89d5135cfd21
Denis Rybkin <rybki001@umn.edu>
2022-08-31 16:18:24 -0400

    Time: 283 ms (95.90%), Space: 15.6 MB (55.16%) - LeetHub

---
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -0,0 +1,30 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        pacific = collections.deque()
+        atlantic = collections.deque()
+        for i in range(len(heights[0])):
+            pacific.append((0, i))
+            atlantic.append((len(heights) - 1, i))
+        for i in range(1, len(heights)):
+            pacific.append((i, 0))
+            atlantic.append((i - 1, len(heights[0]) - 1))
+        pacificSet = set(pacific)
+        atlanticSet = set(atlantic)
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        while pacific:
+            r, c = pacific.popleft()
+            for d1, d2 in dirs:
+                nR, nC = r+d1, c+d2
+                if nR < 0 or nR >= len(heights) or nC < 0 or nC >= len(heights[0]) or heights[nR][nC] < heights[r][c] or (nR, nC) in pacificSet:
+                    continue
+                pacific.append((nR, nC))
+                pacificSet.add((nR, nC))
+        while atlantic:
+            r, c = atlantic.popleft()
+            for d1, d2 in dirs:
+                nR, nC = r+d1, c+d2
+                if nR < 0 or nR >= len(heights) or nC < 0 or nC >= len(heights[0]) or heights[nR][nC] < heights[r][c] or (nR, nC) in atlanticSet:
+                    continue
+                atlantic.append((nR, nC))
+                atlanticSet.add((nR, nC))
+        return list(pacificSet.intersection(atlanticSet))
