11cb7332b9c95cec3129f07ffd6dfd600fedc361
Denis Rybkin <rybki001@umn.edu>
2022-09-06 22:23:16 -0400

    Time: 109 ms (18.95%), Space: 16.5 MB (30.02%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.py
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.py
@@ -5,6 +5,8 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # Runtime Complexity: O(n), since we nee to visit every node and then turn the result into string
+    # Space Complexity: O(n) to store the string
     def tree2str(self, root: Optional[TreeNode]) -> str:
         string = []
         def dfs(node, string):
@@ -13,15 +15,9 @@ class Solution:
             string.append(str(node.val))
             string.append("(")
             dfs(node.left, string)
-            if (node.right and not node.left) or node.left:
-                string.append(")")
-            else:
-                string.pop()
+            string.append(")") if (node.right and not node.left) or node.left else string.pop()
             string.append("(")
             dfs(node.right, string)
-            if string[-1] == "(":
-                string.pop()
-            else:
-                string.append(")")
+            string.pop() if string[-1] == "(" else string.append(")")
         dfs(root, string)
         return "".join(string)
