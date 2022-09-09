7a532fc7c4fa86f0fd48d2a12840fcf4dfe3abc4
Denis Rybkin <rybki001@umn.edu>
2022-09-08 22:57:17 -0400

    Time: 71 ms (5.35%), Space: 13.8 MB (96.04%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.py
+++ 362-design-hit-counter/362-design-hit-counter.py
@@ -5,20 +5,17 @@ class HitCounter:
 
     def hit(self, timestamp: int) -> None:
         self._last300sec(timestamp)
-        if self.seconds and timestamp == self.seconds[-1][0]:
-            self.seconds[-1][1] += 1
-        else:
-            heapq.heappush(self.seconds, [timestamp, 1])
+        heapq.heappush(self.seconds, timestamp)
 
     def getHits(self, timestamp: int) -> int:
         self._last300sec(timestamp)
         hits = 0
         for v in self.seconds:
-            hits += v[1]
+            hits += 1
         return hits
         
     def _last300sec(self, timestamp):
-        while self.seconds and self.seconds[0][0] <= timestamp - 300:
+        while self.seconds and self.seconds[0] <= timestamp - 300:
             heapq.heappop(self.seconds)
 
 # Your HitCounter object will be instantiated and called as such:
