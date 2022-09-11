436e604ed6d82f70b55c6ab4c9a6681e2c2b077e
Denis Rybkin <rybki001@umn.edu>
2022-09-11 16:40:24 -0400

    Time: 415 ms (92.71%), Space: 29.6 MB (95.55%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.py
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.py
@@ -1,4 +1,6 @@
 class Solution:
+    # Runtime Complexity: O(n(log n + log k)) - n log n to sort efficiency and n log k to maintain minHeap sorted
+    # Space Complexity: O(n + k) - n for sorted array and k for heap
     def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
         performance = 0
         speedSum = 0
