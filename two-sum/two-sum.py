bbb315b63243e149e74c255e1c19cdd07e45895c
Denis Rybkin <rybki001@umn.edu>
2022-09-11 19:39:18 -0400

    Time: 125 ms, Memory: 15.3 MB - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,4 +1,7 @@
 class Solution:
+    # Runtime Complexity: O(n) to iterate over every element
+    # Space Complexity: O(n) to create a list
+    # Time Spent: 5 mins
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         numToIdx = {}
         for i, num in enumerate(nums):
