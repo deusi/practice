6d976f846ce9249a2b029ce08bf3448000cc6242
Denis Rybkin <rybki001@umn.edu>
2022-09-13 12:32:40 -0400

    Time: 237 ms (26.02%), Space: 14.2 MB (70.76%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.py
@@ -0,0 +1,22 @@
+class Solution:
+    def validUtf8(self, data: List[int]) -> bool:
+        bits = 0
+        fmask = 1 << 6
+        for d in data:
+            mask = 1 << 7
+            if bits == 0:
+                while d > 0 and d & mask:
+                    bits += 1
+                    mask = mask >> 1
+                    
+                if bits == 0:
+                    continue
+                    
+                if bits > 4 or bits == 1:
+                    return False
+                
+            else:
+                if not (d & mask and not (d & fmask)):
+                    return False
+            bits -= 1
+        return bits == 0
