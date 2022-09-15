ee6f12f796afd49feae7c7100e6969ddd412cc79
Denis Rybkin <rybki001@umn.edu>
2022-09-14 22:32:49 -0400

    Time: 3712 ms (6.30%), Space: 33 MB (25.04%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -1,20 +1,16 @@
 class Solution:
     def findOriginalArray(self, changed: List[int]) -> List[int]:
+        n = len(changed)
+        if n % 2:
+            return []
         changed.sort()
         counter = collections.Counter(changed)
         original = []
         for c in changed:
-            if c in counter and c+c in counter:
-                if c+c == 0 and counter[c+c] == 1:
-                    continue
+            if 2*c in counter and counter[c] > 0 and counter[2*c] > 0:
+                if c == 0 and counter[c] == 1:
+                    continue 
                 original.append(c)
-                counter[c+c] -= 1
-                if counter[c+c] == 0:
-                    del counter[c+c]
-                if c in counter:
-                    counter[c] -= 1
-                    if counter[c] == 0:
-                        del counter[c]
-        if len(changed) == 2 * len(original):
-            return original
-        return []
+                counter[c] -= 1
+                counter[2*c] -= 1
+        return original if n == 2 * len(original) else []
