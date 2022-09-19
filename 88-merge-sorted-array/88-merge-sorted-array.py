8fac22d7f2008a3f0accb10cc0a584ade8fce1fa
Denis Rybkin <rybki001@umn.edu>
2022-09-19 11:29:40 -0400

    Time: 40 ms (91.67%), Space: 13.9 MB (85.72%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -1,4 +1,7 @@
 class Solution:
+    # O(n log n) for sorting
+    # O(1) for sorting in place
+    # Total Time: 25 m - tried to come up with O(m+n) solution
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
