f193018fe69aa1f004e3308ea19f91e577acf838
Denis Rybkin <rybki001@umn.edu>
2022-09-19 22:48:04 -0400

    Time: 379 ms (30.18%), Space: 15.6 MB (64.62%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -6,14 +6,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nonZero = 0
-        for right in range(len(nums)):
-            if nums[right] == 0:
-                nonZero = max(nonZero, right)
-                while nonZero < len(nums) and nums[nonZero] == 0:
-                    nonZero += 1
-                if nonZero >= len(nums):
-                    break
-                nums[right], nums[nonZero] = nums[nonZero], nums[right]
-                nonZero += 1
-        return nums
+        left, right = 0, 0
+        while right < len(nums):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            right += 1
