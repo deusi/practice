0a20c2ac7e39a30c95853087ed5c503d27085f14
Denis Rybkin <rybki001@umn.edu>
2022-09-20 12:01:53 -0400

    Time: 216 ms (34.70%), Space: 14.1 MB (56.81%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
@@ -1,4 +1,7 @@
 class Solution:
+    # Runtime Complexity: O(n) to go over the string
+    # Space Complexity: O(1), since there are at most 26 letters to consider
+    # Total Time: 16 m - tried 3 different implementations
     def firstUniqChar(self, s: str) -> int:
         counter = collections.Counter(s)
         for i, char in enumerate(s):
