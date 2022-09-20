34c1cad5b7b09406879764e9361896accbfce6c4
Denis Rybkin <rybki001@umn.edu>
2022-09-19 22:33:40 -0400

    Time: 480 ms (20.65%), Space: 15.6 MB (64.62%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,5 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort(key = lambda a: a == 0)
-        
+        nonZero = 0
+        for right in range(len(nums)):
+            nonZero = max(nonZero, right)
+            if nums[right] == 0:
+                while nonZero < len(nums) and nums[nonZero] == 0:
+                    nonZero += 1
+                if nonZero >= len(nums):
+                    break
+                nums[right], nums[nonZero] = nums[nonZero], nums[right]
+                nonZero += 1
+        return nums
