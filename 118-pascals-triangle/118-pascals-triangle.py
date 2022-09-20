7f23f7feacb39e4e3c2e7edaf21ed87e719dd111
Denis Rybkin <rybki001@umn.edu>
2022-09-19 21:02:06 -0400

    Time: 62 ms (18.07%), Space: 13.8 MB (66.28%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -0,0 +1,13 @@
+class Solution:
+    # Runtime Complexity: O(n^2) - since we need to generate previous rows to get to current one, i.e row 5 = 5 + 4 + 3 + 2 + 1
+    # Space Complexity: O(n^2)
+    # Time Taken: 17 m
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+        for i in range(numRows):
+            newRow = [1 for _ in range(i + 1)]
+            for j in range(i + 1):
+                if j - 1 >= 0 and j < len(triangle[-1]):
+                    newRow[j] = triangle[-1][j - 1] + triangle[-1][j]
+            triangle.append(newRow)
+        return triangle
