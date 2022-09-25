34b339c1b46a5ac8109b7b505b7cf128cc6d6b8d
Denis Rybkin <rybki001@umn.edu>
2022-09-25 18:00:29 -0400

    Time: 65 ms (41.61%), Space: 15.5 MB (27.70%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -4,12 +4,11 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    # Runtime Complexity: O(n)
-    # Space Complexity: O(1)
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev, cur = None, head
         while cur:
-            next = cur.next
+            tmp = cur.next
             cur.next = prev
-            prev, cur = cur, next
+            prev = cur
+            cur = tmp
         return prev
