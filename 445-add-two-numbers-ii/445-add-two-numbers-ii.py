4a2082121be83cf3f8ca12da35977d944bddc5ce
Denis Rybkin <rybki001@umn.edu>
2022-09-25 22:49:37 -0400

    Time: 77 ms (88.09%), Space: 14 MB (45.59%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.py
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.py
@@ -4,29 +4,38 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    # Runtime Complexity: O(max(m, n)) to create stacks and add numbers
-    # Space Complexity: O(max(m,n)) to store stack
-    # Total Time: 14 m
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        stack1, stack2 = [], []
-        while l1 or l2:
-            if l1:
-                stack1.append(l1.val)
-                l1 = l1.next
-            if l2:
-                stack2.append(l2.val)
-                l2 = l2.next
+        num = 0
+        l1Len, l2Len = 0, 0
+        ptr1, ptr2 = l1, l2
+        while ptr1:
+            l1Len += 1
+            ptr1 = ptr1.next
+        while ptr2:
+            l2Len += 1
+            ptr2 = ptr2.next
+        ptr1, ptr2 = l1, l2
+        while l1Len > 0 and l2Len > 0:
+            num *= 10
+            if l1Len >= l2Len:
+                num += ptr1.val
+                ptr1 = ptr1.next
+                l1Len -= 1
+            if l2Len > l1Len:
+                num += ptr2.val
+                ptr2 = ptr2.next
+                l2Len -= 1
+        if num == 0:
+            return ListNode(0)
         prev = None
-        rem = 0
-        while rem or stack1 or stack2:
-            num1, num2 = 0, 0
-            if stack1:
-                num1 = stack1.pop()
-            if stack2:
-                num2 = stack2.pop()
-            num = num1 + num2 + rem
-            rem = num // 10
-            cur = ListNode(num % 10, prev)
+        while num:
+            cur = ListNode(num % 10)
+            cur.next = prev
             prev = cur
-            
+            num = num // 10
         return prev
+            
+            
+            
+            
+            
