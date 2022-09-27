542805467cdfa3b29501999381b1283dc629a1d8
Denis Rybkin <rybki001@umn.edu>
2022-09-26 21:44:20 -0400

    Time: 66 ms (64.58%), Space: 17 MB (25.82%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -5,16 +5,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # Runtime Complexity O(n)
-    # Space Complexity: O(n)
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        stack = [(root, -math.inf, math.inf)]
-        while stack:
-            node, minVal, maxVal = stack.pop()
-            if node.val >= maxVal or node.val <= minVal:
+        minVal = -inf
+        def inorder(node):
+            nonlocal minVal
+            if not node:
+                return True
+            left = inorder(node.left)
+            if node.val <= minVal:
                 return False
-            if node.right:
-                stack.append((node.right, node.val, maxVal))
-            if node.left:
-                stack.append((node.left, minVal, node.val))
-        return True
+            minVal = node.val
+            right = inorder(node.right)
+            return left and right
+        return inorder(root)
