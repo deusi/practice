32be8f845e9fc9aaafbcd6af3451ca21c638207d
Denis Rybkin <rybki001@umn.edu>
2022-09-26 21:13:03 -0400

    Time: 81 ms (13.71%), Space: 13.8 MB (99.15%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -1,14 +1,18 @@
 class Solution:
-    # O(n log n) for sorting
-    # O(n) for sorting in place
-    # Total Time: 25 m - tried to come up with O(m+n) solution with O(1) space complexity
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
         if not n:
             return
-        for i in range(n):
-            nums1[m+i] = nums2[i]
-        nums1.sort()
-        return
+        left, right, end = m - 1, n - 1, len(nums1) - 1
+        while right >= 0:
+            if left >= 0 and nums1[left] > nums2[right]:
+                nums1[end] = nums1[left]
+                left -= 1
+            else:
+                nums1[end] = nums2[right]
+                right -= 1
+            end -= 1
+                
+        
