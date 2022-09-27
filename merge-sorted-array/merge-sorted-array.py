dea7d3f65fdf2f73a79bc7cff253347c0f95b7c8
Denis Rybkin <rybki001@umn.edu>
2022-09-26 21:15:19 -0400

    Time: 64 ms, Memory: 14 MB - LeetHub

---
+++ merge-sorted-array/merge-sorted-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    # Runtime Complexity: O(n)
+    # Space Complexity: O(1)
+    # Total Time: 12 m
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if not n:
+            return
+        left, right, end = m - 1, n - 1, len(nums1) - 1
+        while right >= 0:
+            if left >= 0 and nums1[left] > nums2[right]:
+                nums1[end] = nums1[left]
+                left -= 1
+            else:
+                nums1[end] = nums2[right]
+                right -= 1
+            end -= 1
