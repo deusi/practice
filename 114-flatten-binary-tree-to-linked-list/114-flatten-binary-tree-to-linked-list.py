96a14283251a19e1fbb70a79af7ef22bbdfe3e31
Denis Rybkin <rybki001@umn.edu>
2022-09-28 13:40:09 -0400

    Time: 78 ms (20.51%), Space: 15.3 MB (45.65%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
@@ -5,9 +5,9 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # Runtime Complexity: O(n) to go over every node to add it to inorder list and n to traverse the list and set up the pointers
-    # Space Complexity: O(n) to keep the list of nodes
-    # Total Time: 17 m
+    # Runtime Complexity: O(n)
+    # Space Complexity: O(n) due to recursion stack
+    # Total Time: 17 m +30m for recursive approach
     def flatten(self, root: Optional[TreeNode]) -> None:
         """
         Do not return anything, modify root in-place instead.
