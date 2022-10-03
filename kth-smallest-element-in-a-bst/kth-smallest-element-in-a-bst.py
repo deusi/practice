d1fac6532d305a23f169ee5f68de76579ec164e5
Denis Rybkin <rybki001@umn.edu>
2022-10-02 21:11:03 -0400

    Time: 121 ms, Memory: 18.1 MB - LeetHub

---
+++ kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # Runtime Complexity: O(n), since the node is the rightmost leaf in the worst case
+    # Space Complexity: O(n) due to stack recursion
+    # Total Time: 20m (for rec sol)
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        count = 0
+        def findElem(node):
+            nonlocal count
+            if not node:
+                return 0
+            ans = 0
+            if node.left and count < k:
+                ans += findElem(node.left)
+            count += 1
+            if count == k:
+                return node.val
+            if node.right and count < k:
+                ans += findElem(node.right)
+            return ans
+        return findElem(root)
+        
