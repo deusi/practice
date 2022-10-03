a309a1ea3f3ce401637b653dee620dec4ffa6370
Denis Rybkin <rybki001@umn.edu>
2022-10-03 18:28:42 -0400

    Time: 38 ms (80.27%), Space: 13.9 MB (71.55%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,36 +1,20 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        def bt(i):
-            st = ""
+        def decode(i):
+            strList, numList = [], []
             while i < len(s):
-                n = 0
-                if s[i] == ']':
-                    return i, st
-                elif s[i].isdigit():
-                    while s[i].isdigit():
-                        n *= 10
-                        n += int(s[i])
-                        i += 1
-                    i, t = bt(i + 1)
-                    st += n * t
+                if s[i] == '[':
+                    i, retStr = decode(i + 1)
+                    num = int("".join(numList))
+                    numList = []
+                    strList.append(num * retStr)
+                if i < len(s) and s[i] == ']':
+                    return i + 1, "".join(strList)
+                if i < len(s) and s[i].isdigit():
+                    numList.append(s[i])
                 else:
-                    st += s[i]
-                i += 1
-            return i, st
-            
-        idx = 0
-        decoded = ""
-        while idx < len(s):
-            num = 0
-            if s[idx].isdigit():
-                while s[idx].isdigit():
-                    num *= 10
-                    num += int(s[idx])
-                    idx += 1
-                idx, temp = bt(idx + 1)
-                print(num, temp)
-                decoded += num * temp
-            else:
-                decoded += s[idx]
-            idx += 1
-        return decoded
+                    if i < len(s):
+                        strList.append(s[i])
+                i += 1 
+            return "".join(strList)
+        return decode(0)
