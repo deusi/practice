bae0210572be25674e96d318eeb45e70642f761d
Denis Rybkin <rybki001@umn.edu>
2022-10-03 18:30:01 -0400

    Time: 65 ms (11.51%), Space: 13.8 MB (71.55%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -8,13 +8,12 @@ class Solution:
                     num = int("".join(numList))
                     numList = []
                     strList.append(num * retStr)
-                if i < len(s) and s[i] == ']':
-                    return i + 1, "".join(strList)
-                if i < len(s) and s[i].isdigit():
+                elif s[i] == ']':
+                    return i, "".join(strList)
+                elif s[i].isdigit():
                     numList.append(s[i])
                 else:
-                    if i < len(s):
-                        strList.append(s[i])
+                    strList.append(s[i])
                 i += 1 
             return "".join(strList)
         return decode(0)
