1d3646d93a089f7e6f7ba76b4b6a64a7e431f4d6
Denis Rybkin <rybki001@umn.edu>
2022-10-05 13:52:20 -0400

    Time: 56 ms, Memory: 14 MB - LeetHub

---
+++ valid-parentheses/valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    # Runtime Complexity: O(n), since we go over every element
+    # Space Complexity: O(n), since stack can grow to the size of n
+    # Total Time 15 m + 7 m to make it look prettier
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pMap = {'(': ')', '[': ']', '{': '}'}
+        for p in s:
+            if p in pMap:
+                stack.append(p)
+            elif not stack or p != pMap[stack[-1]]:
+                return False
+            else:
+                stack.pop()
+        return len(stack) == 0
+            
+            
