fb0b4d760a521457b07851d6a4d10cb837e37776
Denis Rybkin <rybki001@umn.edu>
2022-10-06 22:42:04 -0400

    Time: 170 ms, Memory: 17.4 MB - LeetHub

---
+++ meeting-rooms-ii/meeting-rooms-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    # Runtime Complexity: O(n log n) for sorting
+    # Space Complexity: O(n) to maintain heap
+    # Total Time: 22 m
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        minHeap = []
+        intervals.sort()
+        heapq.heappush(minHeap, intervals[0][1])
+        for s, e in intervals[1:]:
+            if s < minHeap[0]:
+                heapq.heappush(minHeap, e)
+            else:
+                heapq.heapreplace(minHeap, e)
+        return len(minHeap)
+        
