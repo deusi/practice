af96f0248fa735cebb252af7ba9bedf2d108ac24
Denis Rybkin <rybki001@umn.edu>
2022-10-17 20:20:43 -0400

    Time: 84 ms (13.18%), Space: 14.9 MB (28.98%) - LeetHub

---
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.py
@@ -0,0 +1,33 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    # Runtime Complexity: O(n), where n is the length of the flattened list
+    # Space Complexity: O(k), where k is the depth of multilevel ll
+    # Total Time: 22 m
+    def flatten(self, head: 'Node') -> 'Node':
+        def recFlatten(node):
+            prev = None
+            while node:
+                if node.child:
+                    flattened = recFlatten(node.child)
+                    flattened.next = node.next
+                    if node.next:
+                        node.next.prev = flattened
+                    node.next = node.child
+                    node.child.prev = node
+                    node.child = None
+                    #node = flattened
+                prev = node
+                node = node.next
+            return prev
+        recFlatten(head)
+        return head
+        
