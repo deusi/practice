9a1d1e151ef48572a5a555892081f39406b237f4
Denis Rybkin <rybki001@umn.edu>
2022-10-19 22:41:48 -0400

    Time: 42 ms (85.21%), Space: 14.3 MB (39.75%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+        def generatePar(o, c, curPar):
+            if len(curPar) == 2 * n:
+                result.append("".join(curPar))
+                return
+            if o > 0:
+                curPar.append('(')
+                generatePar(o - 1, c, curPar)
+                curPar.pop()
+            if c > o:
+                curPar.append(')')
+                generatePar(o, c - 1, curPar)
+                curPar.pop()
+            return
+        generatePar(n, n, [])
+        return result
