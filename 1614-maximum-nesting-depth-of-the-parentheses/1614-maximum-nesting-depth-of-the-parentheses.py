1e820af47a39e63fdda409474b6daf5d5cbcb8e9
Denis Rybkin <rybki001@umn.edu>
2022-10-20 21:17:18 -0400

    Time: 68 ms (13.18%), Space: 13.8 MB (96.06%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
@@ -1,4 +1,7 @@
 class Solution:
+    # Runtime Complexity: O(n)
+    # Space Complexity: O(n)
+    # Total Time: 5 m
     def maxDepth(self, s: str) -> int:
         stack = []
         maxDepth = 0
