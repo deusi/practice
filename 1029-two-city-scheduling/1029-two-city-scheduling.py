afbf7b26cc2f9db557e9073cf5e165c6d117593f
Denis Rybkin <rybki001@umn.edu>
2022-10-23 14:28:16 -0400

    Time: 86 ms (29.58%), Space: 14 MB (8.76%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,4 +1,7 @@
 class Solution:
+    # Runtime Complexity: O(n log n) for sorting
+    # Space Complexity: O(n) for sorting algorithm
+    # Total Time: 8 m
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         n = len(costs) // 2
         costs.sort(key=lambda x: x[0] - x[1])
