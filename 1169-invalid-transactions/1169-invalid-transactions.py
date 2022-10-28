82172c78359088897e35dc00876f81f805e9f24e
Denis Rybkin <rybki001@umn.edu>
2022-10-27 21:22:11 -0400

    Time: 715 ms (9.62%), Space: 14.4 MB (82.86%) - LeetHub

---
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -0,0 +1,18 @@
+class Solution:
+    def invalidTransactions(self, transactions: List[str]) -> List[str]:
+        invalid = [False for _ in range(len(transactions))]
+        names, times, prices, cities = [], [], [], []
+        for trans in transactions:
+            n, t, p, c = trans.split(',')
+            names.append(n), times.append(int(t)), prices.append(int(p)), cities.append(c)
+        for i in range(len(transactions)):
+            if prices[i] > 1000:
+                invalid[i] = True
+            for j in range(i+1, len(transactions)):
+                if names[i] == names[j] and abs(times[j] - times[i]) <= 60 and cities[j] != cities[i]:
+                    invalid[i], invalid[j] = True, True
+        result = []
+        for i in range(len(invalid)):
+            if invalid[i]:
+                result.append(transactions[i])
+        return result
